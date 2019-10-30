f465ca7ee4be60538cd390ad43aaabe2e64c9957
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-10-29 23:26:51 -0500

    final

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var topics = ["call of duty", "Apex legends", "counter strike", "team fortress"];
+    var topics = ["call of duty", "Apex legends", "counter strike", "team fortress", "resident evil", "fortnite", "metal gear solid", "need for speed", "witcher", "grand theft auto", "skyrim", "tetris", "company of heroes"];
     var search = "";
 
     function searchSet(arg) {
@@ -18,15 +18,43 @@ $(document).ready(function() {
     function renderImages(response) {
         var newTopic = $("#imageHolder");
         newTopic.html("");
-        for (var i = 0; i < 11; i++) {
-            var newSpan = $("<img>").attr("id", "image" + i);
-            newSpan.attr("animated", response.data[i].images.fixed_height.url);
-            newSpan.attr("still", response.data[i].images.fixed_height_still.url);
-            newSpan.attr("src", response.data[i].images.fixed_height_still.url)
-            newTopic.append(newSpan);
-            newSpan.addClass("imagesButton");
+        console.log(response);
+        for (var i = 0; i < 10; i++) {
+            var rating = $("<span>");
+            rating.attr("id", "rating" + i);
+            rating.addClass("rating");
+            rating.text("Rating " + response.data[i].rating);
+            newTopic.append(rating);
+            var newImage = $("<img>").attr("id", "image" + i);
+            newImage.attr("animated", response.data[i].images.fixed_height.url);
+            newImage.attr("still", response.data[i].images.fixed_height_still.url);
+            newImage.attr("activeStatus", "false");
+            newImage.attr("src", response.data[i].images.fixed_height_still.url)
+            newImage.addClass("imagesButton");
 
+            $("#rating" + i).append("<br>");
+            $("#rating" + i).append(newImage);
         }
+        $(".imagesButton").on("click", function(event) {
+            var animated = $(this).attr("animated");
+            var still = $(this).attr("still");
+            var status = $(this).attr("activeStatus");
+            console.log(status);
+            if (status === "false") {
+                console.log($(this).attr("src"));
+
+                $(this).attr("src", animated);
+
+                console.log($(this).attr("src"));
+
+                $(this).attr("activeStatus", "true");
+            } else {
+                $(this).attr("src", still);
+                $(this).attr("activeStatus", "false");
+
+            }
+        })
+
     };
 
     function renderButton() {
@@ -41,18 +69,19 @@ $(document).ready(function() {
         }
 
 
-        $("#search").on("click", function(event) {
-            event.preventDefault();
-            var userInput = $("#searchInput").val().trim().toLowerCase();
-            topics.push(userInput);
-            $("#searchInput").val("");
-            renderButton();
-        })
+
         $(".button").on("click", function(event) {
             dataSearchName = $(this).attr("data-name");
             searchSet(dataSearchName);
         });
     }
+    $("#search").on("click", function(event) {
+        event.preventDefault();
+        var userInput = $("#searchInput").val().trim().toLowerCase();
+        topics.push(userInput);
+        $("#searchInput").val("");
+        renderButton();
+    })
 
     renderButton();
 
